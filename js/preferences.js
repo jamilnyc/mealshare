89410e7289caeba80566b38ef69c9bc609f6c59f
Murshed J. Ahmed <mahmed@marvel.com>
2019-04-20 10:52:13 -0400

    feature: added function to find people by preferences

--- js/preferences.js
+++ js/preferences.js
@@ -23,4 +23,27 @@ var MealShareApp = window.MealShareApp || {};
             })
         });
     };
+    
+    MealShareApp.findPeople = function () {
+        MealShareApp.useToken(function (token, accessKey, secretKey, sessionToken) {
+            var bodyParams = {}
+            bodyParams['op'] = 'find'
+            var requestParams = {}
+
+            var newClientCredentials = MealShareApp.getNewClientCredentials()
+            MealShareApp.apiClient = apigClientFactory.newClient(newClientCredentials);
+            
+            MealShareApp.apiClient.userPreferencesPost(requestParams, bodyParams, requestParams).then(function(result) {
+                // TODO: Check response structure
+                console.log(result)
+
+            }).catch(function(result) {
+                console.error('ERROR: Unable to load chat message');
+                console.log(result);
+                if (result.status === 401 || result.status === 403) {
+                    alert('You are not authorized to perform this action!');
+                }
+            })
+        });
+    };
 })();
