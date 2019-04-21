db6299549a2685460d2507d99e9a02b57a87765b
Murshed J. Ahmed <mahmed@marvel.com>
2019-04-21 15:46:50 -0400

    feature: experimented with file uploads

--- js/preferences.js
+++ js/preferences.js
@@ -46,4 +46,29 @@ var MealShareApp = window.MealShareApp || {};
             })
         });
     };
+    
+    MealShareApp.uploadImage = function(encodedImage) {
+        MealShareApp.useToken(function (token, accessKey, secretKey, sessionToken) {
+            var bodyParams = {
+                'op': 'avatar',
+                'encoded_image': encodedImage
+            }
+            var requestParams = {}
+
+            var newClientCredentials = MealShareApp.getNewClientCredentials()
+            MealShareApp.apiClient = apigClientFactory.newClient(newClientCredentials);
+            
+            MealShareApp.apiClient.userPreferencesPost(requestParams, bodyParams, requestParams).then(function(result) {
+                // TODO: Check response structure
+                console.log(result)
+
+            }).catch(function(result) {
+                console.error('ERROR: Unable to load chat message');
+                console.log(result);
+                if (result.status === 401 || result.status === 403) {
+                    alert('You are not authorized to perform this action!');
+                }
+            })
+        });
+    };
 })();
